84bcfde38adc06fdecd9cb9253c263a1f3780b3e
aokuyama <ao980s@gmail.com>
2023-10-15 09:17:16 +0900

    update gen cmd

--- cmd/gen.sh
+++ cmd/gen.sh
@@ -1,3 +1,9 @@
 #/bin/bash
+
+# generate mock
 cd `dirname $0`/../packages/domain
 go generate ./...
+
+# generate prisma
+cd `dirname $0`/../packages/infra
+go run github.com/steebchen/prisma-client-go generate
