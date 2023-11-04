24829bfabe6255b4b6d367440a05c22b94cfb80b
aokuyama <ao980s@gmail.com>
2023-11-04 10:07:37 +0900

    add cors

--- interfaces/http/main.go
+++ interfaces/http/main.go
@@ -2,18 +2,48 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/aokuyama/circle_scheduler-api/interfaces/http/route/event"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	engine := gin.Default()
-	engine.GET("/health_check", func(c *gin.Context) {
+	o := os.Getenv("ALLOW_ORIGIN")
+	if len(o) < 1 {
+		panic("ALLOW_ORIGIN")
+	}
+
+	g := gin.Default()
+
+	g.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			o,
+		},
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		AllowCredentials: true,
+		MaxAge:           24 * time.Hour,
+	}))
+
+	g.GET("/health_check", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"status": "OK",
 		})
 	})
-	engine.GET("/v1/event/:path", event.FetchEvent)
-	engine.Run(":3000")
+	g.GET("/v1/event/:path", event.FetchEvent)
+	g.Run(":3000")
 }
