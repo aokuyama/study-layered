6a2aa8c64fb94289c0544aab611986667dc08638
aokuyama <ao980s@gmail.com>
2023-11-25 11:11:47 +0900

    package router

--- interfaces/http/main.go
+++ interfaces/http/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"net/http"
 	"os"
 	"time"
 
-	"github.com/aokuyama/circle_scheduler-api/interfaces/http/controller/event"
-	"github.com/aokuyama/circle_scheduler-api/interfaces/http/controller/user"
-	"github.com/aokuyama/circle_scheduler-api/interfaces/http/middleware/auth"
+	"github.com/aokuyama/circle_scheduler-api/interfaces/http/route"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -42,29 +39,7 @@ func main() {
 		MaxAge:           24 * time.Hour,
 	}))
 
-	g.GET("/health_check", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"status": "OK",
-		})
-	})
-
-	v1user := g.Group("/v1/user")
-	v1user.Use(auth.Middleware)
-	{
-		v1user.GET("me", user.Me)
-		v1user.GET("refresh", user.Refresh)
-	}
-
-	g.POST("/v1/user/signup", user.Signup)
-	g.POST("/v1/user/auth", user.Auth)
-	g.GET("/v1/e/:path", event.FetchEvent)
-
-	v1event := g.Group("/v1/event")
-	v1event.Use(auth.Middleware)
-	{
-		v1event.POST(":id/member", event.JoinEvent)
-		v1event.DELETE(":id/member", event.LeaveEvent)
-	}
+	route.Define(g)
 
 	g.Run(":3000")
 }
